7b56785ab59945dc60fced8f15e13c2be61fce73
John Musiu <johnmusiu@gmail.com>
2019-08-13 03:30:11 +0300

    coverage upload fix

--- spec/models/ideas_spec.rb
+++ spec/models/ideas_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Idea, type: :model do
     idea = Idea.create!(name: "My Awesome Idea Name")
     expect(idea.name).to eq("My Awesome Idea Name")
   end
+
+  it "has a description" do
+    idea = Idea.create!(name: "My Awesome Idea Name", description: "Awesome")
+    expect(idea.description).to eq("Awesome")
+  end
 end
